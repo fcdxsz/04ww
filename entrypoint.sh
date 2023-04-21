979255cb44e4cbaa3de6e1cdedb66f1a76b67e6e
fcdxsz <130425524+fcdxsz@users.noreply.github.com>
2023-04-21 04:44:04 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'12a01de0-c410-4dd5-86db-bdc618303351'}
+UUID=${UUID:-'39710fab-746b-4a1e-87cf-fa46ace1d2c4'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
